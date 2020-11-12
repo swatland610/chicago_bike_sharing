0c05c8b180946649783f080604ab9a54272b808b
Sean Watland <51850964+swatland610@users.noreply.github.com>
2020-11-11 21:59:35 -0600

    fix community area numbers and address loop label

--- chicago_community_areas.py
+++ chicago_community_areas.py
@@ -20,6 +20,9 @@ def pull_community_areas_data():
     # fix formatting of community area number
     community_area_w_pop['Number[8]'] = community_area_w_pop['Number[8]'].apply(lambda x: int(x))
 
+    # Fix formatting of "(The) Loop[11]"
+    community_area_w_pop['Name[8]'][31] = "Loop"
+
     # print completed time
     print(pull_community_areas_data.__name__, " completed in ", time.time() - start_time)
 
--- divvy_stations.py
+++ divvy_stations.py
@@ -1,6 +1,10 @@
 import requests
 import time
 import pandas as pd
+import rtree
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+from shapely import wkt
 
 def get_divvy_station_data():
     # start time for time keeping
@@ -12,14 +16,30 @@ def get_divvy_station_data():
 
     divvy_stations_json = pd.read_json(r.text)
     divvy_stations = pd.DataFrame(divvy_stations_json)
+    # Convert to GeoDataFrame to correct community areas
+    divvy_stations = gpd.GeoDataFrame(divvy_stations, geometry=gpd.points_from_xy(divvy_stations.longitude, divvy_stations.latitude))
 
     # Fix :computed_region column names
-    divvy_stations.rename(columns={':@computed_region_vrxf_vc4k':'community_area'}, inplace=True)
+    divvy_stations.rename(columns={':@computed_region_vrxf_vc4k':'old_community_area'}, inplace=True)
 
     # Drop stations outside of the city as there is no community area number
-    divvy_stations = divvy_stations[~divvy_stations['community_area'].isna()]
+    divvy_stations = divvy_stations[~divvy_stations['old_community_area'].isna()]
+
+    # Bring in Chicago Shapefiles for Community Area Comparison
+    chicago = grab_community_area_shapefiles()
+
+    # Fix Community Area
+    divvy_stations['community_area'] = divvy_stations['geometry'].apply(lambda x: find_polygon(chicago, x))
 
     # print completed time
     print(get_divvy_station_data.__name__, " completed in ", time.time() - start_time)
 
     return divvy_stations
+
+def grab_community_area_shapefiles():
+    chicago = gpd.read_file("divvy_data/Boundaries - Community Areas", crs = {'init': 'epsg:4326'})
+    return chicago
+
+def find_polygon(gdf, coordinate):
+    dex = gdf[gdf['geometry'].contains(coordinate)].index.values[0]
+    return gdf['area_num_1'][dex]
